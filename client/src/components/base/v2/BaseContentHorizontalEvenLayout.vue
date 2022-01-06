9ca76651c1f47014ce59a42a3e8c1139bc2f5da8
Wonder Jung <wonder13662@gmail.com>
2022-01-06 09:28:12 +0900

    [client][#4]수평으로 콘텐츠를 배열하는 컴포넌트 추가

---
+++ client/src/components/base/v2/BaseContentHorizontalEvenLayout.vue
@@ -0,0 +1,38 @@
+<template>
+  <BaseRow>
+    <BaseCol
+      v-for="(item, idx) in items"
+      :key="idx"
+      :cols="cols"
+    >
+      <slot
+        name="item"
+        :item="item"
+      />
+    </BaseCol>
+  </BaseRow>
+</template>
+
+<script>
+import BaseRow from '@/components/base/BaseRow';
+import BaseCol from '@/components/base/BaseCol';
+
+export default {
+  name: 'BaseContentHorizontalEvenLayout',
+  components: {
+    BaseRow,
+    BaseCol,
+  },
+  props: {
+    items: {
+      type: Array,
+      required: true,
+    },
+  },
+  computed: {
+    cols() {
+      return Math.round(12 / this.items.length);
+    },
+  },
+};
+</script>
