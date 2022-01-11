a16801768def9853223794030233b03b6883f497
Wonder Jung <wonder13662@gmail.com>
2022-01-11 18:51:18 +0900

    [client][#4]BaseDatePicker 추가

---
+++ client/src/components/base/v1/BaseDatePicker.vue
@@ -0,0 +1,98 @@
+<template>
+  <v-menu
+    ref="menu"
+    v-model="menu"
+    :close-on-content-click="false"
+    :return-value="date"
+    transition="scale-transition"
+    offset-y
+    min-width="auto"
+  >
+    <template v-slot:activator="{ on, attrs }">
+      <v-text-field
+        :value="date"
+        dense
+        readonly
+        :hide-details="!errorMessage"
+        :error-messages="errorMessage"
+        :error="error"
+        :outlined="outlined"
+        v-bind="attrs"
+        v-on="on"
+      />
+    </template>
+    <v-date-picker
+      :value="date"
+      :min="minDate"
+      :max="maxDate"
+      no-title
+      scrollable
+      @change="onChange"
+    />
+  </v-menu>
+</template>
+
+<script>
+import moment from 'moment';
+import utils from '@/lib/utils';
+
+export default {
+  name: 'BaseDatePicker',
+  props: {
+    date: {
+      type: String,
+      default: utils.getNowYYYYMMDD(),
+      // TODO validator
+    },
+    outlined: {
+      type: Boolean,
+      default: false,
+    },
+    errorMessage: {
+      type: String,
+      default: '',
+    },
+    error: {
+      type: Boolean,
+      default: false,
+    },
+    min: {
+      type: String,
+      default: '',
+    },
+    max: {
+      type: String,
+      default: '',
+    },
+  },
+  data() {
+    return {
+      menu: false,
+    };
+  },
+  computed: {
+    minDate() {
+      if (!this.min) {
+        return '';
+      }
+      return moment(this.min).toISOString();
+    },
+    maxDate() {
+      if (!this.max) {
+        return '';
+      }
+      return moment(this.max).toISOString();
+    },
+  },
+  methods: {
+    onChange(v) {
+      this.$emit('change', v);
+      this.menu = false;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
