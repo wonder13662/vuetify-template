6f6d7f2129fa954244112167dce9dcbafe3119fd
Wonder Jung <wonder13662@gmail.com>
2022-01-13 22:34:22 +0900

    [client][#4]BaseTextArea 추가

---
+++ client/src/components/base/v1/BaseTextArea.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-textarea
+    background-color="grey lighten-5"
+    counter="100"
+    clearable
+    dense
+    no-resize
+    outlined
+    :value="text"
+    :rules="rules"
+    @change="onChange"
+  />
+</template>
+
+<script>
+export default {
+  name: 'BaseTextArea',
+  props: {
+    text: {
+      type: String,
+      default: '',
+    },
+    maxCount: {
+      type: Number,
+      required: true,
+      validator(value) {
+        return value > 0;
+      },
+    },
+  },
+  data() {
+    return {
+      rules: [
+        (value) => (value && value.length > 0) || '내용을 입력해야 합니다.',
+        (value) => (value && value.length <= this.maxCount) || `입력된 내용이 ${this.maxCount}자 이하여야 합니다.`,
+      ],
+    };
+  },
+  methods: {
+    onChange(value) {
+      this.$emit('change-value', value);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
