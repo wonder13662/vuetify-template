f172eb1c16361914e5e346108266507ed07d682e
Wonder Jung <wonder13662@gmail.com>
2022-01-22 21:56:31 +0900

    [client][#6]기본 지도 추가 완료

--- client/src/views/map/index.vue
+++ client/src/views/map/index.vue
@@ -1,12 +1,21 @@
 <template>
   <div>
-    Map
+    <BaseNaverMap
+      :map-id="'map-sample'"
+    />
   </div>
 </template>
 
 <script>
+import BaseNaverMap from '@/components/base/v1/BaseNaverMapV2';
+/*
+H3 Api의 기능을 네이버 맵 위에 표시합니다.
+*/
 export default {
-
+  name: 'Map',
+  components: {
+    BaseNaverMap,
+  },
 };
 </script>
 
