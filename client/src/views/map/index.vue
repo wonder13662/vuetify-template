0f6f855e22784a0c5052e20f638d4cfdf471dccb
Wonder Jung <wonder13662@gmail.com>
2022-01-22 22:33:10 +0900

    [client][#6]기본 레이아웃 추가

--- client/src/views/map/index.vue
+++ client/src/views/map/index.vue
@@ -1,13 +1,30 @@
 <template>
-  <div>
-    <BaseNaverMap
-      :map-id="'map-sample'"
-    />
-  </div>
+  <v-app id="inspire">
+    <v-main>
+      <BaseContentHorizontalLayout
+        col-width-right="600px"
+        divider
+      >
+        <template v-slot:left>
+          <div>
+            <BaseNaverMap
+              :map-id="'map-sample'"
+            />
+          </div>
+        </template>
+        <template v-slot:right>
+          <div>
+            여기에 H3 컨트롤 패널이 들어갑니다
+          </div>
+        </template>
+      </BaseContentHorizontalLayout>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
 import BaseNaverMap from '@/components/base/v1/BaseNaverMapV2';
+import BaseContentHorizontalLayout from '@/components/base/v2/BaseContentHorizontalLayout';
 /*
 H3 Api의 기능을 네이버 맵 위에 표시합니다.
 */
@@ -15,6 +32,7 @@ export default {
   name: 'Map',
   components: {
     BaseNaverMap,
+    BaseContentHorizontalLayout,
   },
 };
 </script>
