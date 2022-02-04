f4937fb342a2e078556b353f8d3b450723590320
Wonder Jung <wonder13662@gmail.com>
2022-02-04 11:39:43 +0900

    [client][#6]BaseTinyTable 추가

---
+++ client/src/components/base/v1/BaseTinyTable.vue
@@ -0,0 +1,31 @@
+<template>
+  <div
+    :class="classObject"
+  >
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BaseTinyTable',
+  props: {
+    border: {
+      type: Boolean,
+    },
+  },
+  computed: {
+    classObject() {
+      return {
+        box: this.border,
+      };
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.box {
+  border: solid 1px #BDBDBD;
+}
+</style>
