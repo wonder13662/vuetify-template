179bb9026abfba81819c61c28270baaa82e3708d
Wonder Jung <wonder13662@gmail.com>
2022-03-16 16:55:07 +0900

    [client][#15]로딩프로그레스바 추가

---
+++ client/src/components/base/BaseOverlayProgressCircular.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-overlay
+    :value="isShow"
+    :z-index="zIndex"
+  >
+    <v-progress-circular
+      indeterminate
+    />
+  </v-overlay>
+</template>
+
+<script>
+export default {
+  name: 'BaseOverlayProgressCircular',
+  props: {
+    isShow: {
+      type: Boolean,
+    },
+    zIndex: {
+      type: Number,
+      default: 110,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
