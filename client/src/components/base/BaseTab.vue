c5b9fdeeb337f4bc20694015c416fd880f546b6a
Wonder Jung <wonder13662@gmail.com>
2022-04-08 15:27:31 +0900

    [client][#15]BaseTab 추가

---
+++ client/src/components/base/BaseTab.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-tabs
+    color="indigo"
+    grow
+    slider-size="4"
+    @change="onChange"
+  >
+    <v-tab
+      v-for="v in names"
+      :key="v"
+    >
+      {{ v }}
+    </v-tab>
+    <v-tab-item
+      v-for="(v, idx) in names"
+      :key="v"
+    >
+      <slot
+        name="item"
+        :idx="idx"
+      />
+    </v-tab-item>
+  </v-tabs>
+</template>
+
+<script>
+export default {
+  name: 'BaseTab',
+  props: {
+    names: {
+      type: Array,
+      required: true,
+    },
+  },
+  methods: {
+    onChange(v) {
+      this.$emit('change', v);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
