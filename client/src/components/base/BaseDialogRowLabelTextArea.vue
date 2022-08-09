39bb56bf9e5ca7844cafe61e86fafe50a686012c
Wonder Jung <wonder13662@gmail.com>
2022-08-09 23:53:55 +0900

    [client][#15]Base:다이얼로그의 Label+TextArea 컴포넌트추가

---
+++ client/src/components/base/BaseDialogRowLabelTextArea.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <div class="pa-1">
+      <BaseText
+        bold
+        :text="label"
+      />
+    </div>
+    <div class="pa-1">
+      <BaseTextArea
+        :text="text"
+        :max-count="maxCount"
+        @change="onChange"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import BaseText from '@/components/base/BaseText';
+import BaseTextArea from '@/components/base/BaseTextArea';
+
+export default {
+  name: 'BaseDialogRowLabelTextArea',
+  components: {
+    BaseText,
+    BaseTextArea,
+  },
+  props: {
+    label: {
+      type: String,
+      required: true,
+    },
+    text: {
+      type: String,
+      required: true,
+    },
+    maxCount: {
+      type: Number,
+      required: true,
+    },
+  },
+  methods: {
+    onChange(v) {
+      this.$emit('change', v);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
